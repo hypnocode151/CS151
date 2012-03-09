7a649d4eefd0896a5b37d1c76ed5b1cb3d2f5f37
Zane Melcho <d4rkfr4g@hotmail.com>
2012-03-09 00:30:52 -0800

    Zane 3rd Try

---
+++ Assignment3/src/assignment3/Human.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3;
+
+/**
+ *
+ * @author D4rkFr4g
+ */
+public class Human {
+    
+}
