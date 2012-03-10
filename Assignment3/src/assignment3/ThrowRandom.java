5c3197e16acedca90c37e434a90e889f21f35f21
Tin Tran <hanbaoquan1589@gmail.com>
2012-03-09 22:06:21 -0800

    Friday

--- Assignment3/src/assignment3/ThrowRandom.java
+++ Assignment3/src/assignment3/ThrowRandom.java
@@ -3,16 +3,31 @@
  * and open the template in the editor.
  */
 package assignment3;
-
+import java.util.Random;
 /**
  *
  * @author D4rkFr4g
  */
-public class ThrowRandom {
-    
-    public ThrowRandom()
+public class ThrowRandom implements ThrowCalculator {
+    private Random rand = new Random();
+   
+    public Throw generateThrow()
     {
-        
+        switch(randomthrow(1,3))
+        {
+            case 1: return Throw.ROCK;
+            case 2: return Throw.PAPER;
+            case 3: return Throw.SCISSORS;
+           
+        }
+        return Throw.NULL;
     }
     
+    
+    public int randomthrow(int min, int max)
+    {
+        if(min>max)
+        {return -1;}
+        return rand.nextInt(max-min+1) + min;
+    }
    }
