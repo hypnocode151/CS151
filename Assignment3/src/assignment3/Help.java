cadda67d231bcb66e4e1144cda23548b46cda649
Zane Melcho <d4rkfr4g@hotmail.com>
2012-03-09 21:44:20 -0800

    help

---
+++ Assignment3/src/assignment3/Help.java
@@ -0,0 +1,19 @@
+package assignment3;
+
+/**
+ *
+ * @author D4rkFr4g
+ */
+public class Help 
+{
+    public static void displayHelp() {
+        System.out.printf("\nCmds:\n"
+            + "%10s - throw rock, paper, or scissors\n"
+            + "%10s - display this help message\n"
+            + "%10s - display the current score\n"
+            + "%10s - count the throws so far and the limit\n"
+            + "%10s - quit this program"
+            + "\n"
+            , "r/p/s", "help", "score", "count", "quit");
+    }
+}
