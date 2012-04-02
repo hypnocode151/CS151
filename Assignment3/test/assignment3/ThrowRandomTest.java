90adad12fc236c571cf231d8fc763d10fb00d02b
Zane Melcho <d4rkfr4g@hotmail.com>
2012-04-02 00:18:07 -0700

    Merged

--- Assignment3/test/assignment3/ThrowRandomTest.java
+++ Assignment3/test/assignment3/ThrowRandomTest.java
@@ -7,20 +7,8 @@ import static org.junit.Assert.*;
  *
  * @author ttran
  */
-<<<<<<< HEAD
-public class ThrowRandomTest {
-    protected Throw ROCK;
-    protected Throw PAPER;
-    protected Throw SCISSORS;
-    protected Throw NULL;
-    public ThrowRandomTest() {
-    }
-
-
-=======
 public class ThrowRandomTest 
 {
->>>>>>> 019066683e50eb602b6ff7b1acf290a89516fbd9
     /**
      * Test of generateThrow method, of class ThrowRandom.
      */
@@ -29,16 +17,12 @@ public class ThrowRandomTest
     {
         System.out.println("generateThrow");
         ThrowRandom athrow = new ThrowRandom();
-        Throw expResult = ROCK;
-        Throw expResult2 = PAPER;
-        Throw expResult3 = SCISSORS;
+        Throw expResult = Throw.ROCK;
+        Throw expResult2 = Throw.PAPER;
+        Throw expResult3 = Throw.SCISSORS;
         Throw result = athrow.generateThrow();
-<<<<<<< HEAD
         assertEquals(expResult, result);
         assertEquals(expResult2, result);
         assertEquals(expResult3, result);
-=======
-        assertEquals(expResult, result);    
->>>>>>> 019066683e50eb602b6ff7b1acf290a89516fbd9
     }
 }
