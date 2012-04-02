238d1b0773c21b57c29ae92b369f61fc2746887b
Tin Tran <hanbaoquan1589@yahoo.com>
2012-04-02 00:32:12 -0700

    junit

--- Assignment3/test/assignment3/MatchTest.java
+++ Assignment3/test/assignment3/MatchTest.java
@@ -29,7 +29,32 @@ public class MatchTest
     public void testDeclareWinner() 
     {
         System.out.println("Test declareWinner");
-        Match match = new Match(1,1);
+        Match match = new Match(3,1);
+        Player human = new Human("human");
+        Player computer = new Computer("comp",CalculatorType.RANDOM);
+        human.makeThrow(Throw.ROCK);
+        computer.makeThrow(Throw.ROCK);
+        computer.compareThrows(human);
+        int score1 = computer.getScore();
+        int score2 = human.getScore();
+        
+        human.makeThrow(Throw.PAPER);
+        computer.makeThrow(Throw.ROCK);
+        human.incrementScore();
+        score1 = computer.getScore();
+        score2 = human.getScore();
+        
+        human.makeThrow(Throw.SCISSORS);
+        computer.makeThrow(Throw.ROCK);
+        computer.incrementScore();
+        score1 = computer.getScore();
+        score2 = human.getScore();
+        
+       human.makeThrow(Throw.SCISSORS);
+        computer.makeThrow(Throw.PAPER);
+        human.incrementScore();
+        score1 = computer.getScore();
+        score2 = human.getScore();
         match.declareWinner();
     }
 
