463c3402931fa47a21e4e3543022718c5d034967
hypnocode151 <hanbaoquan1589@gmail.com>
2012-04-03 14:12:12 +0800

    Update Assignment3/src/assignment3/ThrowHistorian.java

--- Assignment3/src/assignment3/ThrowHistorian.java
+++ Assignment3/src/assignment3/ThrowHistorian.java
@@ -1,31 +1,63 @@
 package assignment3;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 /**
  *
  * @author Hypnocode
  */
-public class ThrowHistorian 
-{
-
-    static void recordThrows(Throw aThrow, Throw aThrow0) 
+public class ThrowHistorian {
+        private ArrayList<Throw> recordThrow = new ArrayList<Throw>(); 
+    
+        
+    public void throwRecord(Player Human, Player Computer) {
+        recordThrow.add(Human.getThrow());
+        recordThrow.add(Computer.getThrow());
+    }
+    
+    
+        public ArrayList getLastSequence(int n){
+        ListIterator itr = recordThrow.listIterator();
+        ArrayList newSequence = new ArrayList();
+        
+        if(recordThrow.size() < n) {
+         newSequence = null;   
+        }
+        
+        for(int i = 0 ; i < n ; i++) {
+            newSequence.add(itr.previous());
+        }
+        
+                
+        return newSequence;
+    }
+   
+    
+    public int searchSequence(ArrayList<Throw> Sequence) {
+        ListIterator itr = recordThrow.listIterator(); //makes iterator for 
+                                                //arraylist of total record
+        int counter = 0; //sets counter
+        Throw temp;
+        if(recordThrow.size() < Sequence.size()) {
+         Sequence = null;   
+        }
+        
+        while(itr.hasNext()) {
+            for(int i = 0 ; i < recordThrow.size() ; i = i + 2) {
+                for(int j = 0 ; j < Sequence.size() ; j++) {
+                    if(recordThrow.get(i).equals(Sequence.get(i))) {
+                    counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+    
+     static void reset() 
     {
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
-    static void reset() 
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    static ArrayList<Throw> getLastSequence(int n) 
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    static int searchSequence(ArrayList<Throw> throwList) 
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
 }
+ 
+ 
