b289518b0be3b18c7132edfd49636d3f819177be
Zane Melcho <d4rkfr4g@hotmail.com>
2012-04-03 00:41:32 -0700

    Fix formatting for ThrowHistorian

--- Assignment3/src/assignment3/ThrowHistorian.java
+++ Assignment3/src/assignment3/ThrowHistorian.java
@@ -1,65 +1,76 @@
 package assignment3;
+
 import java.util.ArrayList;
 import java.util.ListIterator;
+
 /**
  *
  * @author Hypnocode
  */
-public class ThrowHistorian {
-        private ArrayList<Throw> recordThrow = new ArrayList<Throw>(); 
-    
-        
-     /*
-     * Saves the throw records into arraylist
-     */
-     public void recordThrows(Throw human, Throw computer) {
-        recordThrow.add(human.getThrow());
-        recordThrow.add(computer.getThrow());
+public class ThrowHistorian 
+{
+    private static ArrayList<Throw> recordThrow = new ArrayList<Throw>(); 
+
+    /*
+    * Saves the throw records into arraylist
+    */
+    public void recordThrows(Throw human, Throw computer) 
+    {
+        recordThrow.add(human);
+        recordThrow.add(computer);
     }
-    
     
     /*
      * retrieves the last n elements from the array
      * @return the last n elements.
      */
-        public static ArrayList getLastSequence(int n){
-        ArrayList<Throw> recordThrow = new ArrayList<Throw>();
+    public static ArrayList getLastSequence(int n)
+    {
         ListIterator itr = recordThrow.listIterator();
         ArrayList newSequence = new ArrayList();
         int start;
-        
-        if(recordThrow.size() < n) {
+
+        if(recordThrow.size() < n) 
+        {
             newSequence = new ArrayList();
             start = recordThrow.size() - n;
-                for(int i = 0 ; i < n ; i++) {
-                    newSequence.add(recordThrow.get(start));
-                    start++;
-                }
-      }           
+
+            for(int i = 0 ; i < n ; i++) 
+            {
+                newSequence.add(recordThrow.get(start));
+                start++;
+            }
+        }           
+
         return newSequence;
     }
-   
     
     /*
      * Searches the ArrayList to find if there are matches with
      * a selected pattern that has previously been seen
      * @return the highest counter
      */
-     public static int searchSequence(ArrayList<Throw> Sequence) {
-        ArrayList<Throw> recordThrow = new ArrayList<Throw>();
+    public static int searchSequence(ArrayList<Throw> Sequence) 
+    {
         ListIterator itr = recordThrow.listIterator(); //makes iterator for 
                                                 //arraylist of total record
         int temp_counter = 0; //sets counter
         int counter = 0;
         Throw temp;
-        if(recordThrow.size() < Sequence.size()) {
-         Sequence = null;   
+        
+        if(recordThrow.size() < Sequence.size()) 
+        {
+            Sequence = null;   
         }
         
-        while(itr.hasNext()) {
-            for(int i = 0 ; i < recordThrow.size() ; i = i + 2) {
-                for(int j = 0 ; j < Sequence.size() ; j++) {
-                    if(recordThrow.get(i).equals(Sequence.get(i))) {
+        while(itr.hasNext()) 
+        {
+            for(int i = 0 ; i < recordThrow.size() ; i = i + 2) 
+            {
+                for(int j = 0 ; j < Sequence.size() ; j++) 
+                {
+                    if(recordThrow.get(i).equals(Sequence.get(i))) 
+                    {
                         temp_counter++;
                         if(temp_counter % Sequence.size() == 0)
                             counter++;
@@ -68,6 +79,7 @@ public class ThrowHistorian {
                 }
             }
         }
+        
         return counter;
     }
     
@@ -75,7 +87,7 @@ public class ThrowHistorian {
     /*
      * Clears the array so that it can be reused in another round.
      */
-     public void reset() 
+    public void reset() 
     {
         recordThrow.clear();
     }
