1dabeac59550e2988a0000d6b6645bbb5c71c82b
Zane Melcho <d4rkfr4g@hotmail.com>
2012-04-02 18:30:05 -0700

    computerTest update

--- Assignment3/src/assignment3/ThrowHistorian.java
+++ Assignment3/src/assignment3/ThrowHistorian.java
@@ -5,10 +5,17 @@ import java.util.Iterator;
  *
  * @author Hypnocode
  */
-public class ThrowHistorian {
+public class ThrowHistorian 
+{
 
-   
+    static void recordThrows(Throw aThrow, Throw aThrow0) 
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 
-    
-    
+    static void reset() 
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
 }
--- Assignment3/test/assignment3/ComputerTest.java
+++ Assignment3/test/assignment3/ComputerTest.java
@@ -15,29 +15,31 @@ import static org.junit.Assert.*;
 public class ComputerTest 
 {
     String name;
-    CalculatorType type = CalculatorType.RANDOM;
-
+    CalculatorType type;
+    
     /**
      * Test of makeThrow method, of class Computer.
      */
     @Test
-    public void testMakeThrow() 
+    public void testRandomMakeThrow() 
     {
-        System.out.println("Test makeThrow");
+        type = CalculatorType.RANDOM;
+         
+        System.out.println("Test RandomMakeThrow");
         Computer comp = new Computer(name, type);
         
         comp.makeThrow(null);
-        runAssert(comp.getThrow());
+        randomAssert(comp.getThrow());
         
         comp.makeThrow(null);
-        runAssert(comp.getThrow());
+        randomAssert(comp.getThrow());
         
         comp.makeThrow(null);
-        runAssert(comp.getThrow());
+        randomAssert(comp.getThrow());
         
     }
     
-    private void runAssert(Throw compThrow)
+    private void randomAssert(Throw compThrow)
     {
         if (compThrow == Throw.ROCK)
             assertEquals(Throw.ROCK, compThrow);
@@ -45,5 +47,97 @@ public class ComputerTest
             assertEquals(Throw.PAPER, compThrow);
         if (compThrow == Throw.SCISSORS)
             assertEquals(Throw.SCISSORS, compThrow);
+    }
+    
+    @Test
+    public void testSmartMakeThrow()
+    {
+        type = CalculatorType.SMART;
+        System.out.println("Test SmartMakeThrow");
+        Computer computer = new Computer(name, type);
+        
+        setupThrowHistorian(1);
+        ThrowHistorian.reset();
+        computer.makeThrow(null);
+        assertEquals(Throw.ROCK, computer.getThrow());
+        
+        setupThrowHistorian(2);
+        ThrowHistorian.reset();
+        computer.makeThrow(null);
+        assertEquals(Throw.SCISSORS, computer.getThrow());
+        
+        setupThrowHistorian(3);
+        ThrowHistorian.reset();
+        computer.makeThrow(null);
+        assertEquals(Throw.PAPER, computer.getThrow());
+        
+        setupThrowHistorian(4);
+        ThrowHistorian.reset();
+        computer.makeThrow(null);
+        assertEquals(Throw.SCISSORS, computer.getThrow());
+        
+        setupThrowHistorian(5);
+        ThrowHistorian.reset();
+        computer.makeThrow(null);
+        
+        // Algorithm conditions
+        // Need a ThrowHistorian for each condition.
+        // Case 1: If R > P && R >= S Choose R
+        // Case 2: If R > P && R < S Choose S
+        // Case 3: If P > R && P > S Choose P
+        // Case 4: If P > R && P <= S Choose S
+        // Case 5: Large Random List
+    }
+    
+    private void setupThrowHistorian(int n)
+    {
+        Throw r = Throw.ROCK;
+        Throw p = Throw.PAPER;
+        Throw s = Throw.SCISSORS;
+            
+        switch (n)
+        {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                ThrowHistorian.recordThrows(r, p);
+                ThrowHistorian.recordThrows(s, r);
+                ThrowHistorian.recordThrows(s, p);
+                ThrowHistorian.recordThrows(r, p);
+                ThrowHistorian.recordThrows(s, r);
+                ThrowHistorian.recordThrows(s, p);
+                ThrowHistorian.recordThrows(p, p);
+                ThrowHistorian.recordThrows(r, r);
+                ThrowHistorian.recordThrows(s, s);
+                ThrowHistorian.recordThrows(r, p);
+                ThrowHistorian.recordThrows(r, r);
+                ThrowHistorian.recordThrows(p, p);
+                ThrowHistorian.recordThrows(s, s);
+                ThrowHistorian.recordThrows(p, r);
+                ThrowHistorian.recordThrows(s, p);
+                ThrowHistorian.recordThrows(r, s);
+                ThrowHistorian.recordThrows(p, s);
+                ThrowHistorian.recordThrows(r, p);
+                ThrowHistorian.recordThrows(s, r);
+                ThrowHistorian.recordThrows(p, s);
+                ThrowHistorian.recordThrows(r, s);
+                ThrowHistorian.recordThrows(p, p);
+                ThrowHistorian.recordThrows(r, r);
+                ThrowHistorian.recordThrows(s, s);
+                ThrowHistorian.recordThrows(p, r);
+                ThrowHistorian.recordThrows(r, s);
+                break;
+                
+        }
     }
 }
