b739c39fa75e315d5598003f768a00323e12dba1
Zane Melcho <d4rkfr4g@hotmail.com>
2012-04-18 22:46:10 -0700

    CommandGUI Skeleton

---
+++ Assignment3/src/assignment3/CommandGUIRequest.java
@@ -0,0 +1,20 @@
+package assignment3;
+
+/**
+ * The CommandGUIRequest displays a list of options for the user to choose from
+ * and translates it to the commands of the game
+ * @author HypnoCode
+ */
+public class CommandGUIRequest extends CommandRequestor
+{
+    /*
+     * Reads input from Player then makes a request based on the input.
+     * @return Command
+     */
+    @Override
+    public Command requestCommand() 
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
