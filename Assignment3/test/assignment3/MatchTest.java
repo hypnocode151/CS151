d8c5ffc2a21fb8812764de0f6c20999708471df0
Tin Tran <hanbaoquan1589@yahoo.com>
2012-04-20 01:17:40 -0700

    test

--- Assignment3/test/assignment3/MatchTest.java
+++ Assignment3/test/assignment3/MatchTest.java
@@ -19,9 +19,44 @@ public class MatchTest
     public void testIsMatchOver()
     {
         System.out.println("Test isMatchOver");
-        Match match  = new Match(3,1,RequestType.GUIREQUEST);
-        boolean expResult = true;
+        Match match  = new Match(1,0,RequestType.GUIREQUEST);
+        boolean expResult = false;
         boolean result = match.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match2  = new Match(0,0,RequestType.GUIREQUEST);
+        expResult = true;
+        result = match2.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match3  = new Match(3,0,RequestType.TEXTREQUEST);
+        expResult = false;
+        result = match3.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match4  = new Match(0,1,RequestType.TEXTREQUEST);
+        expResult = true;
+        result = match4.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match5 =  new Match(0,CalculatorType.RANDOM,RequestType.GUIREQUEST);
+        expResult = true;
+        result = match5.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match6 =  new Match(3,CalculatorType.RANDOM,RequestType.TEXTREQUEST);
+        expResult = false;
+        result = match6.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match7 =  new Match(0,CalculatorType.SMART,RequestType.GUIREQUEST);
+        expResult = true;
+        result = match7.isMatchOver();
+        assertEquals(expResult, result);
+        
+        Match match8 =  new Match(3,CalculatorType.SMART,RequestType.TEXTREQUEST);
+        expResult = false;
+        result = match8.isMatchOver();
         assertEquals(expResult, result);
         
         
